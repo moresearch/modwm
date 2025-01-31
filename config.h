ed9fe301979293abf098c4acad900336979b303a
moresearch <uni.m.a.fouad@gmail.com>
2025-01-31 16:50:35 -0500

    bee

--- config.h
+++ config.h
@@ -3,9 +3,10 @@
 /* appearance */
 static const unsigned int gappx = 10; /* gap pixel between windows */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+//static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 //static const char *fonts[]          = { "monospace:size=10" };
 //static const char dmenufont[]       = "monospace:size=10";
 static const char *fonts[]          = { "Iosevka:size=18" };
