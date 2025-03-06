750a800b6cdd29a4f4be32a79e80aeb58caccd02
moresearch <uni.m.a.fouad@gmail.com>
2025-03-06 10:02:26 -0500

    bee

--- config.h
+++ config.h
@@ -60,7 +60,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
         /* symbol     arrange function */
-        { "|M|",      centeredmaster },
+        //{ "|M|",      centeredmaster },
+        { "",      centeredmaster },
         { "[]=",      tile },    /* first entry is default */
         { "><>",      NULL },    /* no layout function means floating behavior */
         { "[M]",      monocle },
