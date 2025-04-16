8d86d081e1199ebe70a6f8910c374e421dc321c2
moresearch <uni.m.a.fouad@gmail.com>
2025-04-16 00:22:08 -0400

    bee

--- config.h
+++ config.h
@@ -38,7 +38,8 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "1", "2", "3", "4", "5"};
+//static const char *tags[] = { "1", "2", "3", "4", "5"};
+static const char *tags[] = { "🢗", "🢗", "🢗", "🢗", "🢗", "🢗"};
 //static const char *tags[] = { "1", "2", "3"};
 
 static const Rule rules[] = {
