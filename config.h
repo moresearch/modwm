6cd3009f3b318915d50917e8a94d50be2c12d7b7
moresearch <uni.m.a.fouad@gmail.com>
2025-07-09 11:06:09 -0400

    bee

--- config.h
+++ config.h
@@ -39,7 +39,7 @@ static const char *colors[][3]      = {
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 //static const char *tags[] = { "1", "2", "3", "4", "5"};
-static const char *tags[] = { "🢗", "🢗", "🢗", "🢗", "🢗", "🢗"};
+static const char *tags[] = { "🢗", "🢗", "🢗", "🢗", "*", "🢗", "🢗", "🢗", "🢗" };
 //static const char *tags[] = { "1", "2", "3"};
 
 static const Rule rules[] = {
@@ -62,11 +62,12 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
         /* symbol     arrange function */
         //{ "|M|",      centeredmaster },
-        { "",      centeredmaster },
-        { "[]=",      tile },    /* first entry is default */
-        { "><>",      NULL },    /* no layout function means floating behavior */
-        { "[M]",      monocle },
-        { ">M>",      centeredfloatingmaster },
+        { " ",      centeredmaster },
+        //{ "",      centeredmaster },
+        //{ "[]=",      tile },    /* first entry is default */
+        //{ "><>",      NULL },    /* no layout function means floating behavior */
+        //{ "[M]",      monocle },
+        //{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -102,10 +103,10 @@ static const Key keys[] = {
         { MODKEY,                       XK_Tab,    view,           {0} },
         { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
         { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-        { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-        { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-        { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-        { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+        //{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+        //{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+        //{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+        //{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
         { MODKEY,                       XK_space,  setlayout,      {0} },
         { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
         { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
